fa0ba850507a3b5340d79bc0715ba3e13ad4d1fa
Patrick Her <renfu.her@gmail.com>
2024-01-10 15:38:19 +0800

    update information page

--- lib/pages/member/information_page.dart
+++ lib/pages/member/information_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:rc168/main.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 var dio = Dio();
@@ -16,6 +15,28 @@ class InformationPage extends StatefulWidget {
 }
 
 class _InformationPageState extends State<InformationPage> {
+  String htmlData = ''; // 初始化一个变量来存储HTML数据
+
+  @override
+  void initState() {
+    super.initState();
+    fetchInformation();
+  }
+
+  Future<void> fetchInformation() async {
+    try {
+      var response = await dio.get(
+        '${app_url}/index.php?route=extension/module/api/gws_information&information_id=${widget.informationId}&api_key=${api_key}',
+      );
+      var data = response.data['informations'][0]; // 假设我们只关心第一个信息
+      setState(() {
+        htmlData = data['description']; // 将HTML数据设置到状态变量中
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,9 +45,13 @@ class _InformationPageState extends State<InformationPage> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Text('相關說明'),
-      ),
+      body: htmlData.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Html(data: htmlData, style: {
+                'img': Style(backgroundColor: Colors.grey, width: Width(395))
+              }),
+            ),
     );
   }
 }
